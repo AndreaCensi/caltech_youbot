ad9e07d6e95c8ecd53ffaee41fae0a4a9211b8b1
Andrea Censi <andrea@cds.caltech.edu>
2013-03-28 22:32:46 -0700

    more conf

---
+++ ros/youbot_agent/launch/1303/unicorn-record-hhcc.sh
@@ -0,0 +1,29 @@
+
+rosbag record -j -o /ros-logs/ylA  \
+/arm_1/arm_controller/position_command \
+/arm_1/arm_controller/velocity_command \
+/arm_1/gripper_controller/position_command \
+/cam_back/camera_info \
+/cam_back/image_raw/compressed \
+/cam_back/image_raw/compressed/parameter_descriptions \
+/cam_back/image_raw/compressed/parameter_updates \
+/cam_eye_right/camera_info \
+/cam_eye_right/image_raw/compressed \
+/cam_eye_right/image_raw/compressed/parameter_descriptions \
+/cam_eye_right/image_raw/compressed/parameter_updates \
+/clicked_point \
+/cmd_vel \
+/diagnostics \
+/hokuyo_H1204906/parameter_descriptions \
+/hokuyo_H1204906/parameter_updates \
+/hokuyo_H1205005/parameter_descriptions \
+/hokuyo_H1205005/parameter_updates \
+/initialpose \
+/joint_states \
+/move_base_simple/goal \
+/odom \
+/rosout \
+/rosout_agg \
+/scan_hokuyo_H1204906 \
+/scan_hokuyo_H1205005 \
+/tf
